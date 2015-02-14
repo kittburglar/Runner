65e4926f3547dce790760e0d999a598c1de3698e
Kittiphong Xayasane <kxayasan@gmail.com>
2015-02-14 08:19:16 -0500

    start button added to main menu and gameplay scene added

--- Classes/GameplayScene.cpp
+++ Classes/GameplayScene.cpp
@@ -1,29 +1,29 @@
-#include "HelloWorldScene.h"
+#include "GameplayScene.h"
 
 USING_NS_CC;
 
-Scene* HelloWorld::createScene()
+Scene* GameplayScene::createScene()
 {
     // 'scene' is an autorelease object
     auto scene = Scene::create();
     
     // 'layer' is an autorelease object
-    auto layer = HelloWorld::create();
-
+    auto layer = GameplayScene::create();
+    
     // add layer as a child to scene
     scene->addChild(layer);
-
+    
     // return the scene
     return scene;
 }
 
-void HelloWorld::jump(Sprite* s){
-auto moveBy = MoveBy::create(2, Vec2(50,10));
-s->runAction(moveBy);
+void GameplayScene::jump(Sprite* s){
+    auto moveBy = MoveBy::create(2, Vec2(50,10));
+    s->runAction(moveBy);
 }
 
 // on "init" you need to initialize your instance
-bool HelloWorld::init()
+bool GameplayScene::init()
 {
     //////////////////////////////
     // 1. super init first
@@ -34,76 +34,96 @@ bool HelloWorld::init()
     
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
+    
     /////////////////////////////
     // 2. add a menu item with "X" image, which is clicked to quit the program
     //    you may modify it.
-
+    
     // add a "close" icon to exit the progress. it's an autorelease object
     auto closeItem = MenuItemImage::create(
                                            "CloseNormal.png",
                                            "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+                                           CC_CALLBACK_1(GameplayScene::menuCloseCallback, this));
     
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
+    closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
                                 origin.y + closeItem->getContentSize().height/2));
-
+    
     // create menu, it's an autorelease object
     auto menu = Menu::create(closeItem, NULL);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
-
+    
     /////////////////////////////
     // 3. add your codes below...
-
+    
     // add a label shows "Hello World"
     // create and initialize a label
     
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    auto label = Label::createWithTTF("Gameplay Scene", "fonts/Marker Felt.ttf", 24);
     
     // position the label on the center of the screen
     label->setPosition(Vec2(origin.x + visibleSize.width/2,
                             origin.y + visibleSize.height - label->getContentSize().height));
-
+    
     // add the label as a child to this layer
     this->addChild(label, 1);
-
+    
     // add "HelloWorld" splash screen"
     auto sprite = Sprite::create("HelloWorld.png");
-
+    
     // position the sprite on the center of the screen
     sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
+    
     // add the sprite as a child to this layer
     this->addChild(sprite, 0);
     
-    //start button
-    auto startButton = MenuItemImage::create("Bluebtn.png","Bluebtn.png", CC_CALLBACK_1(HelloWorld::startButtonCallback, this));
     
-    startButton->setPosition(100,200);
     
-    auto startButtonMenu = Menu::create(startButton, NULL);
-    startButtonMenu->setPosition(Point::ZERO);
-    this->addChild(startButtonMenu, 1);
+    //initialize my sprite
+    auto mySprite = Sprite::create("megaman.png");
+    
+    //  Create a "one by one" touch event listener
+    // (processes one touch at a time)
+    auto listener1 = EventListenerTouchOneByOne::create();
+    
+    // trigger when you push down
+    listener1->onTouchBegan = [=](Touch* touch, Event* event){
+        CCLOG("onTouchBegan");
+        auto moveBy = MoveBy::create(2, Vec2(50,10));
+        mySprite->runAction(moveBy);
+        return true; // if you are consuming it
+    };
+    
+    // trigger when moving touch
+    listener1->onTouchMoved = [](Touch* touch, Event* event){
+        // your code
+    };
+    
+    // trigger when you let up
+    listener1->onTouchEnded = [=](Touch* touch, Event* event){
+        // your code
+    };
+    
+    // Add listener
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(listener1, this);
+    
+    mySprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    this->addChild(mySprite, 0);
+    
     
     return true;
 }
 
-void HelloWorld::startButtonCallback(Ref* pSender){
-    Director::getInstance()->pushScene(GameplayScene::createScene());
-}
 
-
-
-void HelloWorld::menuCloseCallback(Ref* pSender)
+void GameplayScene::menuCloseCallback(Ref* pSender)
 {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
-	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+    MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
     return;
 #endif
-
+    
     Director::getInstance()->end();
-
+    
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
     exit(0);
 #endif
--- Classes/GameplayScene.h
+++ Classes/GameplayScene.h
@@ -1,27 +1,23 @@
-#ifndef __HELLOWORLD_SCENE_H__
-#define __HELLOWORLD_SCENE_H__
+#ifndef __GAMEPLAY_SCENE_H__
+#define __GAMEPLAY_SCENE_H__
 
 #include "cocos2d.h"
-#include "GameplayScene.h"
 
-class HelloWorld : public cocos2d::Layer
+class GameplayScene : public cocos2d::Layer
 {
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
-
+    
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
     
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
-    void startButtonCallback(cocos2d::Ref* pSender);
     
     void jump(cocos2d::Sprite* s);
     // implement the "static create()" method manually
-    
-    
-    CREATE_FUNC(HelloWorld);
+    CREATE_FUNC(GameplayScene);
 };
 
-#endif // __HELLOWORLD_SCENE_H__
+#endif // __GAMEPLAY_SCENE_H__
